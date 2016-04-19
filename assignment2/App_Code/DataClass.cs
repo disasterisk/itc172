ed1c732dc798bcf3d6183aacbea7f4608ee42e9a
disasterisk <disasterisk@users.noreply.github.com>
2016-04-19 11:42:17 -0700

    Upload assignment 2

---
+++ assignment2/App_Code/DataClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// This class will connect to the database
+/// It will have methods to retrieve the Services
+/// It will also retrieve the grants for that service
+/// Jordan Blackmon 2016-04-12
+/// </summary>
+public class DataClass
+{
+    SqlConnection connect;
+    public DataClass()
+    {
+        connect = new SqlConnection(ConfigurationManager.ConnectionStrings["BookReviewConnectionString"].ToString());
+    }
+    public DataTable getAuthor()
+    {
+        DataTable tbl;
+
+        string sql = "SELECT authorkey, authorname FROM Author";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        tbl = ReadData(cmd);
+
+        return tbl;
+    }
+    public DataTable getBooks(int authorKey)
+    {
+        DataTable tbl = null;
+        string sql = "SELECT * FROM book INNER JOIN authorbook ON book.bookkey = authorbook.bookkey WHERE authorkey = @authorkey";
+
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@authorkey", authorKey);
+        tbl = ReadData(cmd);
+
+        return tbl;
+    }
+    private DataTable ReadData(SqlCommand cmd)
+    {
+        SqlDataReader reader;
+        DataTable tbl = new DataTable();
+
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        tbl.Load(reader);
+        reader.Close();
+        connect.Close();
+
+        return tbl;
+    }
+}
---
+++ assignment2/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    DataClass dc = new DataClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        { LoadDropDownList(); }
+        //keeps people from reselecting index 0, which has no key
+        dropdown.Items[0].Attributes.Add("disabled", "true");
+    }
+    protected void LoadDropDownList()
+    {
+        DataTable tbl = dc.getAuthor();
+        dropdown.DataSource = tbl;
+        dropdown.DataTextField = "authorname";
+        dropdown.DataValueField = "authorkey";
+        dropdown.DataBind();
+        dropdown.Items.Insert(0, "Select author");
+    }
+
+    protected void dropdown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        fillGrid();
+    }
+
+    protected void fillGrid()
+    {
+        
+        int key = int.Parse(dropdown.SelectedValue.ToString());
+        DataTable tbl = dc.getBooks(key);
+        grid.DataSource = tbl;
+        grid.DataBind();
+    }
+}
