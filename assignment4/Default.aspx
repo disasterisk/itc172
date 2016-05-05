d1c95c107b45dfd1110179dc3339d2f9c3b209a0
disasterisk <disasterisk@users.noreply.github.com>
2016-05-05 11:15:27 -0700

    Added files via upload

---
+++ assignment4/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="venueDropdown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="venueDropdown_SelectedIndexChanged"></asp:DropDownList>
+            <asp:DropDownList ID="artistDropdown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="artistDropdown_SelectedIndexChanged"></asp:DropDownList>
+            <asp:Button ID="showButton" runat="server" Text="All Shows!" OnClick="showButton_Click"></asp:Button>
+            <asp:DataGrid ID="data" runat="server"></asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ assignment4/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    assignment3Reference.Iassignment3Client showTracker = new assignment3Reference.Iassignment3Client();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            makeArtistDropdown();
+            makeVenueDropdown();
+        }
+        artistDropdown.Items[0].Attributes.Add("disabled", "true");
+        venueDropdown.Items[0].Attributes.Add("disabled", "true");
+    }
+
+    protected void makeArtistDropdown()
+    {
+        string[] artists = showTracker.getArtist();
+        artistDropdown.DataSource = artists;
+        artistDropdown.DataBind();
+        artistDropdown.Items.Insert(0, "Select Artist");
+    }
+    protected void makeVenueDropdown()
+    {
+        string[] venues = showTracker.getVenues();
+        venueDropdown.DataSource = venues;
+        venueDropdown.DataBind();
+        venueDropdown.Items.Insert(0, "Select Venue");
+    }
+
+    protected void venueDropdown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string venue = venueDropdown.SelectedItem.Text;
+        assignment3Reference.show[] shows = showTracker.byVenue(venue);
+        data.DataSource = shows;
+        data.DataBind();
+        venueDropdown.SelectedIndex = 0;
+    }
+    protected void artistDropdown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string artist = artistDropdown.SelectedItem.Text;
+        assignment3Reference.show[] shows = showTracker.byArtist(artist);
+        data.DataSource = shows;
+        data.DataBind();
+        artistDropdown.SelectedIndex = 0;
+    }
+
+    protected void showButton_Click(object sender, EventArgs e)
+    {
+        string[] shows = showTracker.getShows();
+        data.DataSource = shows;
+        data.DataBind();
+    }
+}
